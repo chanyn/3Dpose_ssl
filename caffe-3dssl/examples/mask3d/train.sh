9ec808f4ed7ff70dda4ded18ed0e2739474bed4e
Keze Wang <kezewang@gmail.com>
2019-05-27 11:07:40 -0700

    Update train.sh
    correct typos

--- caffe-3dssl/examples/mask3d/train.sh
+++ caffe-3dssl/examples/mask3d/train.sh
@@ -2,6 +2,6 @@
 
 TOOLS=../../build/tools
 WEIGHTS=../finetune_whole/params/
-MEDOL=.
+MODEL=.
 
 $TOOLS/caffe train -gpu=0 -solver=$MODEL/solver.prototxt -weights=$WEIGHTS/finetune_whole.caffemodel 2>&1 | tee -a train.log
