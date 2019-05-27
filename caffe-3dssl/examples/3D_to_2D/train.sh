f47b18b5c8a8a9dc823fdd08019930265fb196c9
Keze Wang <kezewang@gmail.com>
2019-05-27 11:08:46 -0700

    Update train.sh

--- caffe-3dssl/examples/3D_to_2D/train.sh
+++ caffe-3dssl/examples/3D_to_2D/train.sh
@@ -2,6 +2,6 @@
 
 TOOLS=../../build/tools
 WEIGHTS=../2D_to_3D/params/
-MEDOL=.
+MODEL=.
 
 $TOOLS/caffe train -gpu=0 -solver=$MODEL/solver.prototxt -weights=$WEIGHTS/bilstm_iter_300000.caffemodel 2>&1 | tee -a train.log
