0a5716c8ea06a8f940d71c8b63c9dd05aa5bc189
Keze Wang <kezewang@gmail.com>
2019-05-27 11:09:08 -0700

    Update train.sh

--- caffe-3dssl/examples/finetune_whole/train.sh
+++ caffe-3dssl/examples/finetune_whole/train.sh
@@ -2,6 +2,6 @@
 
 TOOLS=../../build/tools
 WEIGHTS=../3D_to_2D/params/
-MEDOL=.
+MODEL=.
 
 $TOOLS/caffe train -gpu=0 -solver=$MODEL/solver.prototxt -weights=$WEIGHTS/addsrr_iter_300000.caffemodel 2>&1 | tee -a train.log
